b7d6d1f7d637b8c59f10525cae2e1630a84637cb
Adi Chandra Nugraha <55467774+aichan019@users.noreply.github.com>
2021-11-08 10:35:37 +0700

    Add files via upload

---
+++ uputtt/main2.js
@@ -0,0 +1,185 @@
+function start() {
+    Swal.fire({
+        title: 'Assalamualaikum uputt &#128522',
+        background: '#AEE1E1',
+        confirmButtonText: 'Next'
+      })
+      .then((result) => {
+        Swal.fire({
+            title: 'aduduuu denger uputt gak enak badan &#128532',
+            background: '#AEE1E1',
+            confirmButtonText: 'Next'
+          })
+          .then((result) => {
+            Swal.fire({
+                title: 'kenapa bisa sakit put &#128532',
+                background: '#AEE1E1',
+                confirmButtonText: 'Next'
+              })
+              .then((result) => {
+                Swal.fire({
+                    title: 'imun uputtt lagi kurang yaa &#128532',
+                    background: '#AEE1E1',
+                    confirmButtonText: 'Next'
+                  })
+                  .then((result) => {
+                    Swal.fire({
+                        title: 'atau lagi banyak pikiran &#128532',
+                        background: '#AEE1E1',
+                        confirmButtonText: 'Next'
+                      })
+                      .then((result) => {
+                        Swal.fire({
+                            title: 'maaf aduduuu gak tau kenapa kamu sakit',
+                            background: '#AEE1E1',
+                            confirmButtonText: 'Next'
+                          })
+                          .then((result) => {
+                            Swal.fire({
+                                title: 'da aduduuu bukan dokter &#128532',
+                                background: '#AEE1E1',
+                                confirmButtonText: 'Next'
+                              })
+                              .then((result) => {
+                                Swal.fire({
+                                    title: 'aduduuu mah cuman manusia biasa &#128532',
+                                    background: '#AEE1E1',
+                                    confirmButtonText: 'Next'
+                                  })
+                                  .then((result) => {
+                                    Swal.fire({
+                                        title: 'yang peduli sama uputtt &#128522',
+                                        background: '#AEE1E1',
+                                        confirmButtonText: 'Next'
+                                      })
+                                      .then((result) => {
+                                        Swal.fire({
+                                            title: 'uputtt suka buah apa ?',
+                                            background: '#AEE1E1',
+                                            confirmButtonText: 'Next'
+                                          })
+                                          .then((result) => {
+                                            Swal.fire({
+                                                title: 'aduduuu bawain yaa ... &#128522',
+                                                background: '#AEE1E1',
+                                                confirmButtonText: 'Next'
+                                              })
+                                              .then((result) => {
+                                                Swal.fire({
+                                                    title: 'inii buahnya uputt',
+                                                    imageUrl: 'https://cdn0-production-images-kly.akamaized.net/EbX6JLOMM6EDlNRKZhRID72-C4c=/0x1054:1999x2181/1280x720/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3383364/original/072558400_1613969167-shutterstock_1722861355.jpg',
+                                                    imageWidth: 400,
+                                                    imageHeight: 200,
+                                                    background: '#AEE1E1',
+                                                    confirmButtonText: 'Next'
+                                                  })
+                                                  .then((result) => {
+                                                    Swal.fire({
+                                                        title: 'ehh salah &#128522',
+                                                        background: '#AEE1E1',
+                                                        confirmButtonText: 'Next'
+                                                      })
+                                                      .then((result) => {
+                                                        Swal.fire({
+                                                            title: 'ini mah seblak lakar &#128522',
+                                                            background: '#AEE1E1',
+                                                            confirmButtonText: 'Next'
+                                                          })
+                                                          .then((result) => {
+                                                            Swal.fire({
+                                                                title: 'ini baru bener &#128522',
+                                                                imageUrl: 'https://asset-apac.unileversolutions.com/content/dam/unilever/knorr_world/global/bahasa_indonesia_indonesian/photography_and_pictures/jenis-mangga-harum-manis-50548755-jpg.jpg',
+                                                                imageWidth: 400,
+                                                                imageHeight: 200,
+                                                                background: '#AEE1E1',
+                                                                confirmButtonText: 'Next'
+                                                              })
+                                                              .then((result) => {
+                                                                Swal.fire({
+                                                                    title: 'jangan lupa istirahat yang cukup',
+                                                                    html: '<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>' +
+                                                                    '<div class="center">' +
+                                                                    '<lottie-player src="https://assets3.lottiefiles.com/packages/lf20_4fewfamh.json"  background="transparent"  speed="1"  loop autoplay></lottie-player>' +
+                                                                    '</div>',
+                                                                    background: '#AEE1E1',
+                                                                    confirmButtonText: 'Next'
+                                                                  })
+                                                                  .then((result) => {
+                                                                    Swal.fire({
+                                                                        title: 'makan yang banyak &#128522',
+                                                                        html: '<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>' +
+                                                                              '<div class="center">' +
+                                                                              '<lottie-player src="https://assets4.lottiefiles.com/packages/lf20_amafml4w.json"  background="transparent"  speed="1"  loop autoplay></lottie-player>' +
+                                                                              '</div>',
+                                                                        background: '#AEE1E1',
+                                                                        confirmButtonText: 'Next'
+                                                                      })
+                                                                      .then((result) => {
+                                                                        Swal.fire({
+                                                                            title: 'minum obatnya jugaa &#128522',
+                                                                            html: '<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>' +
+                                                                              '<div class="center">' +
+                                                                              '<lottie-player src="https://assets2.lottiefiles.com/packages/lf20_pk5mpw6j.json"  background="transparent"  speed="1"  loop autoplay></lottie-player>' +
+                                                                              '</div>',
+                                                                            background: '#AEE1E1',
+                                                                            confirmButtonText: 'Next'
+                                                                          })
+                                                                          .then((result) => {
+                                                                            Swal.fire({
+                                                                                title: 'kalo masih gak enak badan berobat yaa uputt &#128522',
+                                                                                background: '#AEE1E1',
+                                                                                confirmButtonText: 'Next'
+                                                                              })
+                                                                              .then((result) => {
+                                                                                Swal.fire({
+                                                                                    title: 'ntar aduduuu anterin kalo uputtt lemes &#128522',
+                                                                                    background: '#AEE1E1',
+                                                                                    confirmButtonText: 'Next'
+                                                                                  })
+                                                                                  .then((result) => {
+                                                                                    Swal.fire({
+                                                                                        title: 'syafakillah uputtt &#128522',
+                                                                                        background: '#AEE1E1',
+                                                                                        confirmButtonText: 'Next'
+                                                                                      })
+                                                                                      .then((result) => {
+                                                                                        Swal.fire({
+                                                                                            title: 'nan jeongmal dangsin-eul joh-ahaeyo &#128522',
+                                                                                            html: '<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>' +
+                                                                                            '<div class="center">' +
+                                                                                            '<lottie-player src="https://assets6.lottiefiles.com/packages/lf20_ply8ftem.json"  background="transparent"  speed="1"  loop autoplay></lottie-player>' +
+                                                                                            '</div>',
+                                                                                            background: '#AEE1E1',
+                                                                                            confirmButtonText: 'Next'
+                                                                                          })
+                                                                                          
+                                                                                        })
+                                                                                    })
+                                                                                })
+                                                                            })
+                                                                        })
+                                                                    })
+                                                                })
+                                                            })
+                                                        })
+                                                    })
+                                                })
+                                            })
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+}
+
+Swal.fire({
+    title: 'jadi simak yaa uput sayang',
+    html: '<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>' +
+          '<div class="center">' +
+          '<lottie-player src="https://assets2.lottiefiles.com/datafiles/d9bc9kYC2VttaKb/data.json"  background="transparent"  speed="1"  loop autoplay></lottie-player>' +
+          '</div>',
+    confirmButtonText: 'Next'})
